e417f0a0f0274abf8c9a1d66f9370d9aa53a6aec
Kenneth Tam <ktam@purdue.edu>
2016-06-04 14:01:47 -0400

    Connected main page to a separate page

--- app/src/main/java/com/example/kenny/indycivichack2016/ScheduleActivity.java
+++ app/src/main/java/com/example/kenny/indycivichack2016/ScheduleActivity.java
@@ -1,26 +1,30 @@
 package com.example.kenny.indycivichack2016;
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Kenny on 6/4/2016.
+ */
+public class ScheduleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.schedule_page);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Button mainButton = (Button) findViewById(R.id.mainButton1);
-        mainButton.setOnClickListener(new View.OnClickListener() {
+        Button scheduleButton1 = (Button) findViewById(R.id.scheduleButton1);
+        scheduleButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent init = new Intent(MainActivity.this, ScheduleActivity.class);
+                Intent init = new Intent( ScheduleActivity.this, MainActivity.class);
                 startActivity(init);
             }
         });
